723e38ceb1c65cafc502503b3c4640fe690cfa4b
Georgios-Michail DIMITRIADIS <gdimit@netcompany.com>
2024-05-20 12:55:07 +0300

    final changes

--- calculators/src/App.jsx
+++ calculators/src/App.jsx
@@ -1,10 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header.jsx'
+import UserInput from './components/UserInput.jsx';
+import Results from './components/Results.jsx';
+import React, { useState } from 'react';
 
 function App() {
+
+ 
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+});
+
+const inputIsValid = userInput.duration >= 1;
+
+
+function handleChange(inputIdentifier, newValue ){
+ setUserInput((prevUserInput) => {
+    return{
+        ...prevUserInput,
+        [inputIdentifier]: +newValue
+    }
+ })
+}
+    
   return (
+    <>
   <Header />
+  <UserInput userInput={userInput} onChange={handleChange} />
+  {!inputIsValid && <p className='center'>Please anter Duration greater than zero</p>}
+  {inputIsValid && <Results input={userInput} />}
+  </>
   );
 }
 
